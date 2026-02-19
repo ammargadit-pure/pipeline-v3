ef524eddfb861de66906369371309d0b972079e2
ammar.gadit <ammar.gadit@purevpn..com>
2026-02-20 03:13:09 +0500

    ralph(found-001): auto-commit from iteration 3

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -11,3 +11,20 @@ if(CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_options(-fsanitize=address,undefined)
     add_link_options(-fsanitize=address,undefined)
 endif()
+
+# clang-tidy target
+find_program(CLANG_TIDY_EXE NAMES clang-tidy)
+if(CLANG_TIDY_EXE)
+    add_custom_target(clang-tidy
+        COMMAND ${CLANG_TIDY_EXE}
+            -p ${CMAKE_BINARY_DIR}
+            ${CMAKE_SOURCE_DIR}/src/libota-core/src/*.c
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        COMMENT "Running clang-tidy on libota-core sources"
+    )
+else()
+    add_custom_target(clang-tidy
+        COMMAND ${CMAKE_COMMAND} -E echo "clang-tidy not found, skipping static analysis"
+        COMMENT "clang-tidy not available"
+    )
+endif()
